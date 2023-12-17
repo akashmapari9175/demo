00b49fec365abd963cc3530c96fdd8174e46ede6
Akash Siddeshwar Mapari <154071124+akashmapari9175@users.noreply.github.com>
2023-12-17 21:29:19 +0530

    Delete demojavagithub directory

--- demojavagithub/src/demojavagithub/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package demojavagithub;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("hello world program on git");
-		System.out.println("now it will added successfully");
-	}
-}
